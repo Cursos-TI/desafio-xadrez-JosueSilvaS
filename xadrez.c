9f1659a3f6203a7daf19e289ddb609e9ee758bb6
JosueSilvaS <103978941+JosueSilvaS@users.noreply.github.com>
2025-09-16 20:42:10 +0000

    Nivel aventureiro - Movimentação cavalo

--- xadrez.c
+++ xadrez.c
@@ -41,29 +41,7 @@ int main() {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    printf("\n");
 
 
 
@@ -71,6 +49,18 @@ int main() {
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+
+
+    for( int i = 1 ; i > 0 ; i--){
+        int x = 2;
+        while (x > 0)
+        {
+            printf("Cavalo move: Cima\n");
+            x--;
+        }
+        printf("Cavalo move: Esquerda\n");
+        
+    }
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
